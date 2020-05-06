d611d271f1ddcd3a038bc21e49c84df02ec5b68c
Lon Bitner <lonbitner@gmail.com>
2020-05-06 21:05:07 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
-
+class CashRegister
+  attr_reader :discount
+  attr_accessor :total
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(item, price, quantity = 1)
+   self.total += price * quantity
+  end
+end
