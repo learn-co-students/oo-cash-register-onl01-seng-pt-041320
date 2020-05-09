e9e7b20284ade1b4f8ff8a4826a1dc07315c5a06
Philippe Timothe <philippetimothe@gmail.com>
2020-05-09 20:00:28 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+require 'pry'
 
+class CashRegister
+  
+  attr_accessor :discount, :total 
+  
+  def initialize(total = 0) 
+    @total = total
+    @discount = 20
+  end
+  
+  def total
+    @total 
+  end
+  
+  def add_item
+    
+  end
+  
+  
+  
+end 
