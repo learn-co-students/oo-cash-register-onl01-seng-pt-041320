024342458c0d35ab47f28bb326e0702a4d3aa40a
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-10 14:39:57 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,46 @@
+class CashRegister 
+  attr_accessor :discount, :items, :last_transaction, :total
 
+  def initialize(discount = 0)
+    @items = []
+    @discount = discount
+    @total = 0 
+  end 
+  
+  
+  def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
+    else
+      @items << title
+    end
+    @last_total = @total
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
+  end
+  
+  def apply_discount()
+   if @discount > 0   
+     @discount = @discount/100.to_f
+     @total = @total - (@total * (@discount))
+     "After the discount, the total comes to $#{@total.to_i}."
+      else
+      "There is no discount to apply."
+    end
+  end
+  
+  def void_last_transaction
+    @total = @last_total
+  end
+
+
+end
+
+
+     
+  
